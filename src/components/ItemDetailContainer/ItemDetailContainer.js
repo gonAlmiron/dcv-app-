d12ee370e0781ba141e889f4bc5c69b8acb57fff
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2022-09-07 20:23:38 -0300

    Commit

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react"g
+import {useEffect, useState} from "react"
 import ItemDetail from "../ItemDetail/ItemDetail"
 // import { useParams } from 'react-router-dom'
 import { getItem } from "../../helpers/getItem"
