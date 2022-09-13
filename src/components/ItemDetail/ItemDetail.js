6b24e824f2b80450b909424f62897e7877df5de8
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2022-09-13 11:03:01 -0300

    Main

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-const ItemDetail = ({item}) => {
-=======
 import Item from "../Item/Item"
 import { useParams } from "react-router-dom"
 import ItemCount from "../ItemListContainer/ItemCount";
@@ -9,33 +6,30 @@ import { useState } from "react";
 
 const ItemDetail = ({item}) => {
     let {itemId } = useParams();
->>>>>>> be28ba17e61125185443cdf36da79071011ec812
 
-    const [cantidad, setCantidad] = useState(1)
 
-    const handleAgregar = () => {
-        console.log({
-            ...item,
-            cantidad
-        })
-    }
+    // const [cantidad, setCantidad] = useState(1)
+
+    // const handleAgregar = () => {
+    //     console.log({
+    //         ...item,
+    //         cantidad
+    //     })
+    // }
 
     return (
         <div className="container my-5">
-<<<<<<< HEAD
+
             <img src={item.img}/>
             <h3>{item.nombre}</h3>
             <p>{item.desc}</p>
             <p>{item.category}</p>
             <h4>{item.precio}</h4>
-=======
+
             <h4>Detalle del producto</h4>
 
          <hr/>
             { item.map((prod) => <Item producto={prod} key={prod.id}/>) }   
-            <ItemCount
-            counter={cantidad}/>
->>>>>>> be28ba17e61125185443cdf36da79071011ec812
         </div>
     )
 }
@@ -69,4 +63,3 @@ export default ItemDetail
 // }
 
 // export default ItemDetail
-
